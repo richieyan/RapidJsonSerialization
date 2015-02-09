7113f0a0b3bd9d7e1ff8ef7c46d7a037b4b61e4b
Richie Yan <richie.yan@outlook.com>
2015-02-09 18:12:52 +0800

    Initial Commit

---
+++ TestRapidJson/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestRapidJson
+//
+//  Created by Richie Yan on 2/9/15.
+//  Copyright (c) 2015 Richie Yan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
